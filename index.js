5d4a0d71e7632ad9fdfd84f77cd2b05016a6a16c
Debayan <debayanlodh1904@gmail.com>
2026-01-11 18:09:18 +0530

    The whole project completed

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+const app=express();
+const port=3000;
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+});
+app.post("/unlock",async(req,res)=>{
+    try{
+        const result=await axios.get("https://api.api-onepiece.com/v2/characters/en");
+        const characters=result.data;
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        const character = characters[randomIndex];
+        res.render("index.ejs",{name:character.name,bounty:character.bounty||"unknown"});
+    }catch(error){
+        res.send(404);
+    }
+});
+
+app.listen(port,()=>{
+    console.log("app is runnin");
+});
